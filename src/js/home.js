e98c2a8a9bca9baa2d13020d1922e75db8c872a1
Mallerie Shirley <mallerie@gmail.com>
2016-05-18 14:29:14 -0400

    added title

---
+++ src/js/home.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Link } from 'react-router';
+
+
+export default class Home extends Component {
+  
+  render() {
+
+    return(
+      <div className="home">
+        <h1>Mallerie Shirley's Portfolio</h1>
+      </div>
+      )
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 // Javascript Entry Point
+
+import React, { Component } from 'react';
+import { Route, Router, hashHistory, IndexRoute } from 'react-router';
+import { render } from 'react-dom';
+
+
+
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={Home}/>
+      
+  </Router>
+), document.querySelector('.app'));
+
